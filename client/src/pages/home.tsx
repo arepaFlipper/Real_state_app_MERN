37f1d2f25833d90189f5ae9c08fa976b870b0d4c
cris <ctovar@abexuspr.com>
2023-03-03 13:46:42 -0500

    🐬 (54:01) PieCharts & home

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useList } from "@pankod/refine-core";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
+import { PieChart, PropertyReferrals, TotalRevenue, PropertyCard, TopAgent } from "../components";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div style={{ backgroundColor: "#EBECFD" }}>
+      <Box>
+        <Typography fontSize={25} fontWeight={700} color="#11142D">
+          Dashboard
+        </Typography>
+        <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+          <PieChart title="Properties for Sale" value={684} series={[75, 25]} colors={['#475be8', '#e4a8ef']} />
+          <PieChart title="Properties for Rent" value={550} series={[60, 40]} colors={['#475ae8', '#e4b8ef']} />
+          <PieChart title="Total customers" value={5684} series={[73, 24]} colors={['#275ae8', '#c4b8ef']} />
+          <PieChart title="Properties for Cities" value={555} series={[74, 26]} colors={['#475ae8', '#e4b8ef']} />
+        </Box>
+      </Box>
+    </div>
   )
 }
 
