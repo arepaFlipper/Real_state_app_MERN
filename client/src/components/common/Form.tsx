099a81753f17cabaeb69df0b471da3950f683fa3
cris <ctovar@abexuspr.com>
2023-03-04 07:01:08 -0500

    🙊 (1:33:45) Create Property

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Box, Typography, FormControl, FormHelperText, TextField, TextareaAutosize, Stack, Select, MenuItem, Button } from '@pankod/refine-mui';
+import { FormProps } from 'interfaces/common';
+import CustomButton from './CustomButton';
 
-const Form = () => {
+const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, onFinishHandler, propertyImage }: FormProps) => {
+  const { name, onChange, onBlur, ref } = register('title', { required: true });
   return (
-    <div>Form</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        {type} a property
+      </Typography >
+      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#FCFCFC" >
+        <form style={{ marginTop: '20px', width: '100%', display: 'flex', flexDirection: 'column', gap: '20px' }} onSubmit={handleSubmit(onFinishHandler)}>
+          <FormControl>
+            <FormHelperText sx={{ fontWeight: 500, margin: '10px 0', fontSize: 16, color: '#11142D' }}>Enter property name</FormHelperText>
+            <TextField fullWidth required id="outlined-basic" color="info" style={{ border: "1px solid gray", borderRadius: "6px" }} variant="outlined" name={name} onChange={onChange} onBlur={onBlur} ref={ref} />
+          </FormControl>
+        </form>
+      </Box>
+    </Box>
   )
-}
+};
 
-export default Form
+export default Form;
