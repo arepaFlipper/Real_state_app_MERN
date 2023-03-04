49eb99089d71a369625359fa9a46659f66bee40f
cris <ctovar@abexuspr.com>
2023-03-04 08:55:10 -0500

    🔦 (1:48:30)>add the handler functions

--- client/src/pages/createProperty.tsx
+++ client/src/pages/createProperty.tsx
@@ -10,8 +10,22 @@ const CreateProperty = () => {
   const { data: user } = useGetIdentity();
   const [propertyImage, setPropertyImage] = useState({ name: '', url: '' });
   const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
-  const handleImageChange = () => { };
-  const onFinishHandler = () => { };
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, _) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
+    reader(file).then((result: string) => setPropertyImage({ name: file?.name, url: result }));
+  };
+  const onFinishHandler = async (data: FieldValues) => {
+    if (!propertyImage.name) {
+      return alert('Please select an image')
+    }
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email })
+
+    navigate('/properties');
+  };
   return (
     <Form type="Create" register={register} onFinish={onFinish} formLoading={formLoading} handleSubmit={handleSubmit} handleImageChange={handleImageChange} onFinishHandler={onFinishHandler} propertyImage={propertyImage} />
   )
