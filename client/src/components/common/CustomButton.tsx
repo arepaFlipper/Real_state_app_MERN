d2ac6c1ae0f9d146ea09eb21403ef6b467a7ed3f
cris <ctovar@abexuspr.com>
2023-03-05 06:48:12 -0500

    🟪 (2:34:07)define the type of the CustomButton

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -1,9 +1,25 @@
 import { Button } from '@pankod/refine-mui';
 import { CustomButtonProps } from "interfaces/common";
 
-const CustomButton = ({ title, handleClick, backgroundColor, fullWidth, color, icon }: CustomButtonProps) => {
+const CustomButton = ({ type, title, handleClick, backgroundColor, fullWidth, color, icon }: CustomButtonProps) => {
+  const sx = {
+    flex: fullWidth ? 1 : 'unset',
+    padding: '10px 15px',
+    width: fullWidth ? '100%' : 'fit-content',
+    minWidth: 130,
+    backgroundColor,
+    color,
+    fontSize: 16,
+    fontWeight: 600,
+    gap: '10px',
+    textTransform: 'capitalize',
+    '&:hover': {
+      opacity: 0.9, backgroundColor
+    }
+  }
+  const button_type = type === "submit" ? 'submit' : "button";
   return (
-    <Button onClick={handleClick} sx={{ flex: fullWidth ? 1 : 'unset', padding: '10px 15px', width: fullWidth ? '100%' : 'fit-content', minWidth: 130, backgroundColor, color, fontSize: 16, fontWeight: 600, gap: '10px', textTransform: 'capitalize', '&:hover': { opacity: 0.9, backgroundColor } }}>
+    <Button onClick={handleClick} type={button_type} sx={sx}>
       {icon}
       {title}
     </Button>
