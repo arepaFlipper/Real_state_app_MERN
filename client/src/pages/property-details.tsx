cb3b509a3cabd75974d1fcb19a8cb36bec538bba
cris <ctovar@abexuspr.com>
2023-03-08 16:45:42 -0500

    🔝 (3:22:47) fetch property-details

--- client/src/pages/property-details.tsx
+++ client/src/pages/property-details.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+import { useDelete, useGetIdentity, useShow } from '@pankod/refine-core';
+import { useParams, useNavigate } from '@pankod/refine-react-router-v6';
+import { ChatBubble, Delete, Edit, Phone, Place, Star } from '@mui/icons-material';
+import { CustomButton } from 'components';
 
 const PropertyDetails = () => {
+  const navigate = useNavigate();
+  const { data: user } = useGetIdentity();
+  const { id } = useParams();
+  const { mutate } = useDelete();
+  const { queryResult } = useShow();
+
+  const { data, isLoading, isError } = queryResult;
+  console.log(`🇬🇱%cproperty-details.tsx:15 - data`, 'font-weight:bold; background:#48b700;color:#fff;'); //DELETEME
+  console.log(data); // DELETEME
   return (
     <div>PropertyDetails</div>
   )
