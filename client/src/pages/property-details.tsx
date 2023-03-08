e82ff86976ca905acc82c82e8416a097a46ecbf4
cris <ctovar@abexuspr.com>
2023-03-08 17:32:40 -0500

    ♎ (3:34:25) PropertyDetails component

--- client/src/pages/property-details.tsx
+++ client/src/pages/property-details.tsx
@@ -12,8 +12,45 @@ const PropertyDetails = () => {
   const { queryResult } = useShow();
 
   const { data, isLoading, isError } = queryResult;
+
+  const propertyDetails = data?.data ?? {};
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error</div>
+
   return (
-    <div>PropertyDetails</div>
+    <Box borderRadius="15px" padding="20px" bgcolor="#FCFCFC" width="fit-content">
+      <Typography fontSize={25} fontWeight={700} color="#11142D">Details</Typography>
+      <Box mt="20px" display="flex" flexDirection={{ xs: 'column', lg: 'row' }} gap={4} >
+        <Box flex={1} maxWidth={764}>
+          <img src={propertyDetails.photo} alt={propertyDetails.title} height={546} style={{ objectFit: 'cover', aspectRatio: "1/1", borderRadius: '10px' }} className="property_details-img" />
+          <Box mt="15px">
+            <Stack direction="row" justifyContent="space-between" flexWrap="wrap" alignItems="center">
+              <Typography fontSize={18} fontWeight={500} color="#11142D" textTransform="capitalize" style={{ color: "black" }}>{propertyDetails.propertyType} </Typography>
+              <Box>
+                {[1, 2, 3, 4, 5].map((star) => {
+                  return (
+                    <Star key={`star-${star}`} sx={{ color: '#F2C94C' }} />
+                  )
+                })}
+              </Box>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between" flexWrap="wrap" alignItems="center">
+              <Box>
+                <Typography fontSize={22} fontWeight={600} color="#11142D" textTransform="capitalize" style={{ color: "black" }}>{propertyDetails.title} </Typography>
+                <Stack mt={0.5} direction="row" alignItems="center" gap={0.5}>
+                  <Place sx={{ color: '#808191' }} />
+                  <Typography style={{ color: "#808191" }} >{propertyDetails.location}</Typography>
+                </Stack>
+              </Box>
+              <Box>
+
+              </Box>
+            </Stack>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
