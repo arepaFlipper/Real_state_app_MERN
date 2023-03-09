5eb352b74fb23d5d17e86224a24490f55f2c00eb
cris <ctovar@abexuspr.com>
2023-03-09 06:59:06 -0500

    ☑️  add show to url

--- client/src/components/common/PropertyCard.tsx
+++ client/src/components/common/PropertyCard.tsx
@@ -6,7 +6,7 @@ import { PropertyCardProps } from 'interfaces/property';
 
 const PropertyCard = ({ id, title, location, price, photo }: PropertyCardProps) => {
   return (
-    <Card component={Link} to={`/properties/${id}`} sx={{ maxWidth: '330px', padding: '10px', '&:hober': { boxShadow: '0 22px 45px 2px rgba(176, 176, 176, 0.1)' }, cursor: 'pointer' }} elevation={0} >
+    <Card component={Link} to={`/properties/show/${id}`} sx={{ maxWidth: '330px', padding: '10px', '&:hober': { boxShadow: '0 22px 45px 2px rgba(176, 176, 176, 0.1)' }, cursor: 'pointer' }} elevation={0} >
       <CardMedia component="img" height={210} image={photo} alt="card image" sx={{ borderRadius: '10px', aspectRatio: "1/1" }} />
       <CardContent sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: '10px', padding: '5px' }}>
         <Stack direction="column" gap={1}>
