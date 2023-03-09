7b6eaab53dd4b357af5d0fdb141a02431aabcac4
cris <ctovar@abexuspr.com>
2023-03-09 18:29:52 -0500

    🌡️ (4:13:15) Agents

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useOne } from '@pankod/refine-core';
+import { useParams } from '@pankod/refine-react-router-v6';
+
+import { Profile } from 'components';
 
 const AgentProfile = () => {
+  const { id } = useParams();
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: id as string,
+  });
+
+  const agentProfile = data?.data ?? [];
+
+  if (isLoading) {
+    return <div>Loaging...</div>;
+  }
+
+  if (isError) {
+    return <div>Something went wrong!</div>
+  }
   return (
-    <div>AgentProfile</div>
+    <Profile type="Agent" name={agentProfile?.name} avatar={agentProfile?.avatar} email={agentProfile.email} properties={agentProfile?.allProperties} />
   )
 }
 
